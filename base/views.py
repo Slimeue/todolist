ef68858ce057efe379cb5e345bde4d74a47d8467
Kevinroy <77309706+Slimeue@users.noreply.github.com>
2023-12-10 11:03:10 +0800

    Update: added login logout function

--- base/views.py
+++ base/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import logout
+
 from .models import Task
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = 'tasks'
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+    
+
+def logout_View(request):
+    logout(request)
+    return redirect('login')
+    
+
+
 # Create your views here.
 class TaskList(ListView):
     model = Task
